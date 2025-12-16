dc7912ec0edc4a3a6a37878e49fecb3a460dd0d5
Lesya <dnosbusy@gmail.com>
2025-12-16 19:25:56 +0300

    Add comprehensive tests for 100% code coverage

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,30 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+    @Test
+    public void testReset() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(10.0);
+        agr.addValue(20.0);
+        assertThat(agr.getSum()).isEqualTo(30.0);
+        agr.reset();
+        assertThat(agr.getSum()).isZero();
+        agr.addValue(5.0);
+        assertThat(agr.getSum()).isEqualTo(5.0);
+    }
+
+    @Test
+    public void testAddValueEdgeCases() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(0.0);
+        assertThat(agr.getSum()).isZero();
+        agr.addValue(-5.0);
+        assertThat(agr.getSum()).isEqualTo(-5.0);
+        agr.addValue(10.0);
+        assertThat(agr.getSum()).isEqualTo(5.0);
+        agr.reset();
+        agr.addValue(1.5);
+        agr.addValue(2.5);
+        assertThat(agr.getSum()).isEqualTo(4.0);
+    }
 }
--- src/test/java/testproject/CalculatorTest.java
+++ src/test/java/testproject/CalculatorTest.java
@@ -14,4 +14,20 @@ public class CalculatorTest {
     public void testSubtract() {
         assertThat(Calculator.subtract(3,2)).isEqualTo(1);
     }
+
+    @Test
+    public void testSumEdgeCases() {
+        assertThat(Calculator.sum(0, 0)).isEqualTo(0);
+        assertThat(Calculator.sum(-5, 10)).isEqualTo(5);
+        assertThat(Calculator.sum(100, -100)).isEqualTo(0);
+    }
+
+    @Test
+    public void testSubtractEdgeCases() {
+        assertThat(Calculator.subtract(0, 0)).isEqualTo(0);
+        assertThat(Calculator.subtract(10, 20)).isEqualTo(-10);
+        assertThat(Calculator.subtract(-5, -3)).isEqualTo(-2);
+    }
 }
+
+
